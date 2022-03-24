6cf57e9145720fdc42be7199f22aa15098b11236
smbirch <spencer.m.birch@gmail.com>
2022-03-24 11:32:26 -0700

    implementing Add, Complete, and Delete methods

--- todo.go
+++ todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type item struct {
 	Task        string `json:"task"`
@@ -10,3 +13,40 @@ type item struct {
 }
 
 type List []item
+
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Now(),
+	}
+
+	*l = append(*l, t)
+}
+
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("task %d does not exist", i)
+	}
+
+	// Adjusting for 0 based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("task %d does not exist", i)
+	}
+
+	// Adjusting for 0 based index
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+
+}
