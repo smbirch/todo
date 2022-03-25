d25da0ff97e1466398842dabccb95a2020f59c63
smbirch <spencer.m.birch@gmail.com>
2022-03-25 13:10:35 -0700

    adding integ tests for main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"todo"
 )
 
@@ -15,5 +16,22 @@ func main() {
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	switch {
+	// For no extra args, print the list
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		// concat all args with a space
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+
+		// Save new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 }
